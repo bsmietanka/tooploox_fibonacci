9d168d5e6cf6f9ff3c781e623898adb487a8445f
bsmietanka <bartek.smietanka@gmail.com>
2021-12-10 19:10:31 +0100

    Artistic fibonacci sequence implementation incorporating tooploox logo into the code bitmap

---
+++ overcomplicated_fibonacci.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append("tooploox_logo_overcomplicated_fibonacci.png")
+from tooploox_logo_overcomplicated_fibonacci import fibonacci
+
+if __name__== "__main__":
+    for elem in fibonacci(30):
+        print(elem)
